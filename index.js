7949e372b8507894a4f5b910e57ffdf91517f027
Ola <musicola@mail.ru>
2023-02-08 12:33:14 +0300

    edit practical tasks

---
+++ index.js
@@ -0,0 +1,32 @@
+
+let x=40000;
+let y=22000;
+console.log(x+y);
+
+
+let c='Владивосток';
+let d=2000;
+console.log(c+' '+d);
+
+
+
+let e=128;
+let f=254;
+let a=137;
+let b=201;
+console.log(e+f+a+b);
+
+
+let g=2;
+console.log(g**5);
+
+
+
+let text='Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!';
+console.log(text);
+
+
+const greetings = 'Привет';
+const name = 'Кот';
+const newString = greetings + ',' + ' ' + name;
+console.log(newString);
